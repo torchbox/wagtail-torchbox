499ac03f76f57a87a133a12e66e498f0cd34f6ff
Wes West <wes.west@torchbox.com>
2014-03-07 13:18:05 +0000

    Jobs index styling

--- tbx/settings/base.py
+++ tbx/settings/base.py
@@ -140,6 +140,7 @@ INSTALLED_APPS = (
     # 'django.contrib.sites',  # Wagtail uses its own site management logic
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 
     'south',
     'compressor',
