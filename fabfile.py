90f8437d21e2c22e31bed68869aca8006ed2ca83
Dave Cranwell <david@torchbox.com>
2014-03-12 17:42:26 +0000

    added new fab scripts for uploading media and dbs to staging

--- fabfile.py
+++ fabfile.py
@@ -8,11 +8,27 @@ env.roledefs = {
     'production': ['tbxwagtail@by-web-2.torchbox.com'],
 }
 
+# TODO:
+# DATABASES = {
+#     'staging':{
+#         'name': 'tbxwagtail',
+#         'user': 'tbxwagtail',
+#     }
+#     'production':{
+#         'host': 'by-postgres-a.torchbox.com',
+#         'name': 'wagtail-torchbox',
+#         'user': 'tbxwagtail',
+#     }
+# }
+
+STAGING_DB_USERNAME = "tbxwagtail"
+STAGING_DB_NAME = "tbxwagtail"
 LIVE_DB_USERNAME = "tbxwagtail"
 LIVE_DB_SERVER = "by-postgres-a.torchbox.com"
 DB_NAME = "wagtail-torchbox"
 LOCAL_DUMP_PATH = "/home/vagrant/"
 REMOTE_DUMP_PATH = "~/"
+
 
 @roles('staging')
 def deploy_staging():
@@ -64,22 +80,39 @@ def pull_live_data():
     local('psql -Upostgres %s -f %s' % (DB_NAME, local_path))
     local ('rm %s' % local_path)
 
-@roles('production')
-def push_live_data():
-    filename = "%s-%s.sql" % (DB_NAME, uuid.uuid4())
-    local_path = "%s%s" % (LOCAL_DUMP_PATH, filename)
-    remote_path = "%s%s" % (REMOTE_DUMP_PATH, filename)
+@roles('staging')
+def push_staging_media():
+    media_filename = "%s-%s-media.tar" % ('wagtail-torchbox', uuid.uuid4())
+    local_media_dump = "%s%s" % (LOCAL_DUMP_PATH, media_filename)
+    remote_media_dump = "%s%s" % (REMOTE_DUMP_PATH, media_filename)
+
+    # tar and upload media
+    local('tar -cvf %s media' % local_media_dump)
+    local('gzip %s' % local_media_dump)
+    put(local_media_dump, remote_media_dump)
+
+    # unzip everything
+    with cd('/usr/local/django/tbxwagtail/'):
+        sudo('rm -rf media')
+        sudo('mv %s .' % remote_media_dump)
+        sudo('tar -xzvf %s' % remote_media_dump)
+
+@roles('staging')
+def push_staging_data():
+    db_filename = "%s-%s.sql" % (DB_NAME, uuid.uuid4())
+    local_path = "%s%s" % (LOCAL_DUMP_PATH, db_filename)
+    remote_path = "%s%s" % (REMOTE_DUMP_PATH, db_filename)
     live_db_backup_path = "%s%s-%s.sql" % (REMOTE_DUMP_PATH, DB_NAME, uuid.uuid4())
 
-    local('pg_dump -Upostgres -cf %s %s' % (local_path, DB_NAME))
+    # dump and upload db
+    local('pg_dump -Upostgres -c %s > %s' % (DB_NAME, local_path))
     local('gzip %s' % local_path)
     put("%s.gz" % local_path, "%s.gz" % remote_path)
 
-    run('pg_dump -U%s -h %s -cf %s' % (LIVE_DB_USERNAME, LIVE_DB_SERVER, live_db_backup_path))
+    run('pg_dump -U%s -c %s > %s' % (STAGING_DB_USERNAME, STAGING_DB_NAME, live_db_backup_path))
     puts('Previous live database backed up to %s' % live_db_backup_path)
     
-    run('dropdb -U%s -h %s %s' % (LIVE_DB_USERNAME, LIVE_DB_SERVER, DB_NAME))
-    run('createdb -U%s -h %s %s' % (LIVE_DB_USERNAME, LIVE_DB_SERVER, DB_NAME))
     run('gunzip %s.gz' % remote_path)
-    runs('psql -U%s -h %s -f %s %s' % (LIVE_DB_USERNAME, LIVE_DB_SERVER, remote_path, DB_NAME))
-    run ('rm %s' % remote_path)
+    run('psql -U%s -f %s %s' % (STAGING_DB_USERNAME, remote_path, STAGING_DB_NAME))
+    run('rm %s' % remote_path)
+
