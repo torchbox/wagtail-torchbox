6e4102b7e078302beb5d7bda9ce7f4f40884c6c7
Wes West <wes.west@torchbox.com>
2014-03-13 14:24:25 +0000

    Added share functionality

--- torchbox/static/torchbox/js/main.js
+++ torchbox/static/torchbox/js/main.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
 	$(".menu-button").click(function() {
 			$('nav ul').toggleClass('visible');
 	});
+
+	$(".share a").click(function() {
+		$('.share ul').toggleClass('visible');
+	});
     
 
 });
