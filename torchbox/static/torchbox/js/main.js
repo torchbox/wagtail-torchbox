c9eadc8c9f3ef65e9dacfaa184f133e1b8291485
Wes West <wes.west@torchbox.com>
2014-04-01 15:54:58 +0100

    Added fitvid

--- torchbox/static/torchbox/js/main.js
+++ torchbox/static/torchbox/js/main.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
 
+	// Menu toggle
 	$(".menu-button").click(function() {
 			$('nav ul').toggleClass('visible');
 	});
 
+	// Share hide
 	$(".share a").click(function() {
 		$('.share ul').toggleClass('visible');
 	});
     
+    // fitVids
+    $(".responsive-object").fitVids();
 
 });
