5ee3350508e56d467a2163efd3767149f3826917
Dave Cranwell <david@torchbox.com>
2014-12-18 17:01:06 +0000

    tweaks

--- torchbox/static/torchbox/js/ajax-load.js
+++ torchbox/static/torchbox/js/ajax-load.js
@@ -1,15 +1,17 @@
 $(function() {
 	$(document).on('click', '.next', function(e){
 		e.preventDefault();
+		$.scrollTo($('section.blog'), 1000);
 		$('#listing').load(blog_page, next_params, function(){
-			$.scrollTo({top:'400px', left:'0px'}, 1000);
+			
 		});
 	});
 
 	$(document).on('click', '.previous', function(e){
 		e.preventDefault();
+		$.scrollTo($('section.blog'), 1000);
 		$('#listing').load(blog_page, prev_params, function(){
-			$.scrollTo({top:'400px', left:'0px'}, 1000);
+			
 		});
 	});
 });
