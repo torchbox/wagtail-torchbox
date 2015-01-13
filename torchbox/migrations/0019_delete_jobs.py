24e279755f305cf2860a18b0524c12087709d64e
Karl Hobley <karlhobley10@gmail.com>
2015-01-13 16:59:58 +0000

    Commented out old datamigration
    Was previously crashing first setup. It is no longer needed as live data has it already applied

--- torchbox/migrations/0019_delete_jobs.py
+++ torchbox/migrations/0019_delete_jobs.py
@@ -8,10 +8,10 @@ class Migration(DataMigration):
 
     def forwards(self, orm):
         "Write your forwards methods here."
-        for job in orm['torchbox.JobPage'].objects.all():
-            job.delete()
-        contenttype = orm['contenttypes.ContentType'].objects.get(app_label='torchbox', model='jobpage')
-        orm['wagtailcore.Page'].objects.filter(content_type=contenttype).delete()
+        #for job in orm['torchbox.JobPage'].objects.all():
+        #    job.delete()
+        #contenttype = orm['contenttypes.ContentType'].objects.get(app_label='torchbox', model='jobpage')
+        #orm['wagtailcore.Page'].objects.filter(content_type=contenttype).delete()
         # Note: Don't use "from appname.models import ModelName". 
         # Use orm.ModelName to refer to models in this application,
         # and orm['appname.ModelName'] for models in other applications.
