709663e69e28246bef1a6567cd3cc9f67300eb45
Dave Cranwell <david@torchbox.com>
2015-03-26 10:20:28 +0000

    added migration to include deprecation message

---
+++ torchbox/migrations/0006_auto_20150326_1023.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import wagtail.wagtailcore.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('torchbox', '0005_auto_20150325_1631'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='blogpage',
+            name='body',
+            field=wagtail.wagtailcore.fields.RichTextField(verbose_name='body (deprecated. Use streamfield instead)'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='blogpage',
+            name='intro',
+            field=wagtail.wagtailcore.fields.RichTextField(verbose_name='Intro (deprecated. Use streamfield instead)', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='standardpage',
+            name='body',
+            field=wagtail.wagtailcore.fields.RichTextField(verbose_name='Body (deprecated. Use streamfield instead)', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='standardpage',
+            name='intro',
+            field=wagtail.wagtailcore.fields.RichTextField(verbose_name='Intro (deprecated. Use streamfield instead)', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='workpage',
+            name='body',
+            field=wagtail.wagtailcore.fields.RichTextField(verbose_name='Body (deprecated. Use streamfield instead)', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='workpage',
+            name='intro',
+            field=wagtail.wagtailcore.fields.RichTextField(verbose_name='Intro (deprecated. Use streamfield instead)', blank=True),
+            preserve_default=True,
+        ),
+    ]
