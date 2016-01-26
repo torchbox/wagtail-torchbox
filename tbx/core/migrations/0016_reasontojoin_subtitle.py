297f225fe8c79fdb7082f48a836aa84877c9790a
Tom Talbot <tom.talbot@torchbox.com>
2016-01-26 15:44:55 +0000

    Add a subtitle field to "Reasons To Join"

---
+++ tbx/core/migrations/0016_reasontojoin_subtitle.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('torchbox', '0015_auto_20151105_1715'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='reasontojoin',
+            name='subtitle',
+            field=models.CharField(default='', max_length=255),
+            preserve_default=False,
+        ),
+    ]
