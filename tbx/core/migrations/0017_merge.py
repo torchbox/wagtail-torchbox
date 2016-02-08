4f57da3a428ee76bad28514a61e3718ea3be0ca5
Tom Talbot <tom.talbot@torchbox.com>
2016-02-08 11:34:55 +0000

    Merge migration

---
+++ tbx/core/migrations/0017_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('torchbox', '0016_reasontojoin_subtitle'),
+        ('torchbox', '0016_auto_20160205_1724'),
+    ]
+
+    operations = [
+    ]
