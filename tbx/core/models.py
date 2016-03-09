8791684ef2482d1892f8dc1144ee7deb70f01fd8
Nick Smith <nick.smith@torchbox.com>
2016-03-09 16:10:56 +0000

    #1576 add email notification of ad grants form submissions

--- tbx/core/models.py
+++ tbx/core/models.py
@@ -12,6 +12,7 @@ from django.http import HttpResponse
 from django.forms import ModelForm
 from django.forms.widgets import TextInput
 
+from wagtail.wagtailadmin.utils import send_mail
 from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailcore.fields import RichTextField, StreamField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel, \
@@ -980,6 +981,10 @@ class GoogleAdGrantsPage(Page):
     form_title = models.CharField(max_length=255)
     form_subtitle = models.CharField(max_length=255)
     form_button_text = models.CharField(max_length=255)
+    to_address = models.EmailField(
+        verbose_name='to address', blank=True,
+        help_text="Optional - form submissions will be emailed to this address"
+    )
     body = RichTextField()
     grants_managed_title = models.CharField(max_length=255)
     call_to_action_title = models.CharField(max_length=255)
@@ -1001,6 +1006,11 @@ class GoogleAdGrantsPage(Page):
             form = GoogleAdGrantApplicationForm(request.POST)
             if form.is_valid():
                 form.save()
+
+                if self.to_address:
+                    subject = "{} form submission".format(self.title)
+                    content = '\n'.join([x[1].label + ': ' + str(form.data.get(x[0])) for x in form.fields.items()])
+                    send_mail(subject, content, [self.to_address],)
                 return render(
                     request,
                     'torchbox/includes/ad_grant_application_landing.html',
@@ -1022,6 +1032,7 @@ class GoogleAdGrantsPage(Page):
             FieldPanel('form_title'),
             FieldPanel('form_subtitle'),
             FieldPanel('form_button_text'),
+            FieldPanel('to_address'),
         ], "Application Form"),
         MultiFieldPanel([
             FieldPanel('grants_managed_title'),
