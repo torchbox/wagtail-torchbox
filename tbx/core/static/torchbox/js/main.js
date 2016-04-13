12c32cc55a0e4ff6bd6320ba9997749f70991555
Justine Pocock <justine@torchbox.com>
2016-04-13 15:21:31 +0100

    sorry - frontend stuff - all of it in one go

--- tbx/core/static/torchbox/js/main.js
+++ tbx/core/static/torchbox/js/main.js
@@ -18,6 +18,27 @@ function bindSignUpFormPageForm(element) {
     });
 }
 
+$(function(){
+    //owl // quotes
+    var carousel = $(".carousel");
+
+    $('.carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        pagination: true,
+        paginationNumbers: false,
+        // Navigation
+        navText : [""],
+        responsive:{
+            0:{
+                nav:true,
+                items:1,
+                pagination: true,
+            }
+        }
+    });
+});
 
 
 $(function() {
@@ -196,4 +217,55 @@ $(function() {
 
     //featherlight used for lightboxes in streamfield-enabled pages
     $('.gallery').featherlightGallery();
+
+    $(function(){
+
+        var inOutState = function( trigger, target, speed ){
+
+            var $trigger    = $( trigger ),
+                inClass     = 'mouseEnter',
+                outClass    = 'mouseLeave',
+                resetClass  = 'reset',
+                speed       = speed ? speed : 500,
+                state 		= {
+                    busy : false
+                }
+
+            $trigger.each(function(){
+
+                var $item 	= $( this ),
+                    $target = $item.find( target );
+
+                $item.on( 'mouseenter', function(){
+                    if( state.busy ){ return false; };
+                    $target.addClass( inClass );
+                });
+
+                $item.on( 'mouseleave', function(){
+
+                    state.busy = true;
+                    $target.addClass( outClass );
+
+                    $target.removeClass( inClass );
+                    setTimeout(function(){
+                        $target.addClass( resetClass );
+                        $target.removeClass( outClass );
+                        setTimeout(function(){
+                            $target.removeClass( resetClass );
+                            setTimeout( function(){
+                                state.busy = false;
+                            }, 10);
+                        }, 5);
+                    }, speed)
+
+                });
+
+            });
+
+        };
+
+
+        inOutState( 'button', '.rule' );
+
+    });
 });
