f11f45f37c488b0f68e13084aa9188db6154082d
James Gillen <james.gillen@torchbox.com>
2016-07-05 09:08:01 +0100

    contact page

--- tbx/core/static/torchbox/js/main.js
+++ tbx/core/static/torchbox/js/main.js
@@ -141,10 +141,59 @@ $(function() {
                });
            });
 
-        // match height
-        // $('.jobs li .content').matchHeight();
-
 });
+
+// snazzy maps // contact page
+google.maps.event.addDomListener(window, 'load', init);
+
+function init() {
+  // Basic options for a simple Google Map
+  // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+
+  var mapOptions = {
+    // How zoomed in you want the map to start at (always required)
+    zoom: 4,
+    scrollwheel: false,
+
+    // The latitude and longitude to center the map (always required)
+    center: new google.maps.LatLng(45, -30),
+
+    // How you would like to style the map.
+    // This is where you would paste any style found on Snazzy Maps.
+    styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"lightness":"69"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"administrative.country","elementType":"geometry","stylers":[{"lightness":"35"}]},{"featureType":"administrative.country","elementType":"geometry.fill","stylers":[{"lightness":"1"}]},{"featureType":"administrative.province","elementType":"geometry.fill","stylers":[{"weight":"3.94"},{"lightness":"45"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
+};
+
+
+  // Get the HTML DOM element that will contain your map
+  var mapElement = document.getElementById('map');
+
+  // Create the Google Map
+  var map = new google.maps.Map(mapElement, mapOptions);
+
+  // Adding marker
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(51.858469, -1.480863),
+    map: map,
+    title: 'Oxford',
+    icon: '/static/torchbox/images/pin.png'
+  });
+
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(51.454814, -2.597802),
+    map: map,
+    title: 'Bristol',
+    icon: '/static/torchbox/images/pin.png'
+  });
+
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(39.950865, -75.145590),
+    map: map,
+    title: 'PHILADELPHIA',
+    icon: '/static/torchbox/images/pin.png'
+  });
+
+}
+
 
 // end redesign
 
