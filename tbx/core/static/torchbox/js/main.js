abd84a44fcd83c6fa92109ba0cb44f872608a138
Simon Evans <simon.evans@torchbox.com>
2016-08-25 15:46:47 +0100

    Updated main nav JS and removed dead code

--- tbx/core/static/torchbox/js/main.js
+++ tbx/core/static/torchbox/js/main.js
@@ -3,12 +3,8 @@ $(document).ready(function() {
     tbx.heroImages();
     tbx.mobileMenu();
     tbx.loadMore();
-    tbx.team();
-    tbx.clipThru();
-    tbx.scrollEvents();
     tbx.signUp();
     tbx.jobs();
-    // tbx.map();
 });
 
 var tbx = {
@@ -63,6 +59,8 @@ var tbx = {
     var $bleed            = $( '.bleed' ),
         $bleedItem        = $bleed.find( 'li' ),
         $menuButton       = $( '.menu-button' ),
+        $header           = $( '.container-header' ),
+        headerFixed       = 'container-header--fixed',
         twist             = 'twist',
         bleedVisible      = 'visible out-animation',
         showItem          = 'show';
@@ -71,6 +69,7 @@ var tbx = {
 
       $bleedItem.removeClass( showItem ); 
       $menuButton.removeClass( twist );
+      $header.removeClass( headerFixed );
 
       setTimeout(function() {
         $bleed.removeClass( bleedVisible );
@@ -81,7 +80,7 @@ var tbx = {
     function open( $item ) {
 
       // Open tray
-      $bleed.addClass( bleedVisible )
+      $bleed.addClass( bleedVisible );
       $menuButton.addClass( twist );
 
       // Fade in nav items
@@ -89,6 +88,12 @@ var tbx = {
         $item = $(this);
         $item.addClass( showItem ); 
       });
+
+      // Fix header
+      // Delay for animation
+      setTimeout(function() {
+        $header.addClass( headerFixed );
+      }, 300);
     }
 
     $menuButton.click(function() {
@@ -135,94 +140,6 @@ var tbx = {
         $clientsButton.html( lessLabel );
       }
     });
-  },
-
-
-  // Team filtering
-  team: function() {
-
-    var $textFilter   = $( '.js-text-view' ),
-        $imageFilter  = $( '.js-image-view' ),
-        $peopleList   = $( '.people-list' ),
-        textView      = 'people-list--text',
-        active        = 'active';
-
-    $textFilter.on( 'click', function(){
-      $( this ).addClass( active );
-      $imageFilter.removeClass( active );
-      $peopleList.addClass( textView );
-    });
-
-    $imageFilter.on( 'click', function(){
-      $( this ).addClass( active );
-      $textFilter.removeClass( active );
-      $peopleList.removeClass( textView );
-    });
-  },
-
-
-  // // Scroll events
-  // scrollEvents: function() {
-  //   if ( $( window ).width() > 768 ) {
-  //     $( window ).on( 'scroll', function(){
-
-  //         var $aboutText              = $( '.about-text' ), 
-  //             $heroText               = $( '.hero-text' ),
-  //             scrollTop               = $(window).scrollTop(),
-  //             stopClass               = 'stop';
-
-  //         // Stop hero text
-  //         ( function() {
-  //            if ( scrollTop >= 335 ) {
-  //                $heroText.addClass( stopClass );
-  //            } else {
-  //                $heroText.removeClass( stopClass );
-  //            }
-  //         }());
-
-  //         // Stop about text
-  //         ( function() {
-  //           if ( scrollTop >= 465 ) {
-  //               $aboutText.addClass( stopClass );
-  //           } else {
-  //               $aboutText.removeClass( stopClass );
-  //           }
-  //         }());
-
-  //         // Add padding as page is scrolled
-  //         ( function() {
-  //           var paddingStart          = 0,
-  //               paddingStop           = 285,
-  //               textContent           = $( '.js-text-content' ),
-  //               offset                = Math.abs( $(document).scrollTop() + 60 ),
-  //               paddingStop           = 340,
-  //               paddingTop            = null;
-
-  //           if ( textContent.length ) {
-              
-  //             if ( offset <= paddingStop ) {
-  //               paddingTop = offset;
-  //             }
-
-  //             textContent.css({
-  //               'paddingTop' : paddingTop,
-  //             });
-  //           }
-  //         }());
-  //      });
-  //   }
-  // },
-
-
-  // Clip Thru
-  // https://github.com/salsita/jq-clipthru
-  clipThru: function() {
-    if ( $( '#tester-unique' ).length ) {
-      $( '#tester-unique' ).clipthru({
-        autoUpdate: true,
-        autoUpdateInterval: 30
-      });
-    }
   },
 
 
@@ -398,331 +315,3 @@ var tbx = {
     }
   }
 };
-
-// OLD/NON-REFACTORED JS
-// // hero hover on li change background image
-// $('.featured-case-studies li:nth-child(1)').hover(
-//     function() {
-//         $('.home-hero').addClass('first-feature');
-//     },
-//     function() {
-//         $('.home-hero').removeClass('first-feature');
-//     }
-// );
-
-// $('.featured-case-studies li:nth-child(3)').hover(
-//     function() {
-//         $('.home-hero').addClass('third-feature');
-//     },
-//     function() {
-//         $('.home-hero').removeClass('third-feature');
-//     }
-// );
-
-
-// // hero hover on li change background image
-// $('.featured-case-studies li:nth-child(1)').hover(
-//     function() {
-//         $('.home-hero').addClass('first-feature');
-//     },
-//     function() {
-//         $('.home-hero').removeClass('first-feature');
-//     }
-// );
-
-// $('.featured-case-studies li:nth-child(3)').hover(
-//     function() {
-//         $('.home-hero').addClass('third-feature');
-//     },
-//     function() {
-//         $('.home-hero').removeClass('third-feature');
-//     }
-// );
-
-// end redesign
-
-
-//     var inOutState = function( trigger, target, speed ){
-
-//         var $trigger    = $( trigger ),
-//             inClass     = 'mouseEnter',
-//             outClass    = 'mouseLeave',
-//             resetClass  = 'reset',
-//             speed       = speed ? speed : 500,
-//             state        = {
-//                 busy : false
-//             }
-
-//         $trigger.each(function(){
-
-//             var $item    = $( this ),
-//                 $target = $item.find( target );
-
-//             $item.on( 'mouseenter', function(){
-//                 if( state.busy ){ return false; };
-//                 $target.addClass( inClass );
-//             });
-
-//             $item.on( 'mouseleave', function(){
-
-//                 state.busy = true;
-//                 $target.addClass( outClass );
-
-//                 $target.removeClass( inClass );
-//                 setTimeout(function(){
-//                     $target.addClass( resetClass );
-//                     $target.removeClass( outClass );
-//                     setTimeout(function(){
-//                         $target.removeClass( resetClass );
-//                         setTimeout( function(){
-//                             state.busy = false;
-//                         }, 10);
-//                     }, 5);
-//                 }, speed)
-
-//             });
-
-//         });
-
-//     };
-
-
-//     inOutState( 'button', '.rule' );
-
-// });
-
-// $(function() {
-//     var $window = $(window);
-//     var breakpoints = {
-//         medium: 'screen and (min-width: 45em)',
-//         large: 'screen and (min-width: 96em)'
-//     }
-
-//     Harvey.attach(breakpoints.medium, {
-//         on: function(){
-//             $('body').addClass('breakpoint-medium');
-//         },
-//         off: function(){
-//             $('body').removeClass('breakpoint-medium');
-//         }
-//     });
-//     Harvey.attach(breakpoints.large, {
-//         on: function(){
-//             $('body').addClass('breakpoint-large');
-//         },
-//         off: function(){
-//             $('body').removeClass('breakpoint-large');
-//         }
-//     });
-
-
-//     // Scroll all anchor links
-//     $('a[href*=#]:not([href=#])').click(function() {
-//         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-
-//             var target = $(this.hash);
-//             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-//             if (target.length) {
-//                 $('html,body').animate({
-//                     scrollTop: target.offset().top
-//                 }, 1000);
-//                 return false;
-//             }
-//         }
-//     });
-
-//     // Menu toggle
-//     $(".menu-button").click(function() {
-//         $('nav ul').toggleClass('visible');
-//     });
-
-//     // Share hide
-//     $(".share a").click(function() {
-//         $('.share ul').toggleClass('visible');
-//     });
-
-//     // News-letter PLAY toggle
-//     $('.sign-up').on('click', function() {
-//       $('.newsletter-wrapper-top').toggleClass('show');
-//     });
-
-//     // News-letter PLAY height container
-//     $('.sign-up').on('click', function() {
-//       $('header.container').toggleClass('taller');
-//     });
-
-//     // newsletter signup form
-//     $('.newsletter-signup').on('submit', function(e) {
-//         e.preventDefault();
-//         $(".newsletter-button").html("Signing up...");
-//         $(".newsletter-email").animate({
-//             width: "0px"
-//         });
-//         $(".newsletter-email").hide("slow");
-//         $.ajax({
-//             url : $(this).attr('action'),
-//             type: "GET",
-//             data: $(this).serialize(),
-//             success: function (data) {
-//                 // Google Tag Manager voodoo
-//                 window.dataLayer = window.dataLayer || [];
-//                 window.dataLayer.push({
-//                     'event' : 'formSubmissionSuccess',
-//                     'formId' : 'newsletter-signup'
-//                 });
-//                 // end voodoo
-//                 $(".newsletter-button").html("Thanks!");
-//             }
-//         });
-//     });
-
-//     // Google ad grant application form
-//     $('.grant-application').on('submit', function(e) {
-//         e.preventDefault();
-//         e.stopImmediatePropagation();
-//         $(".grant-application-button").html("Submitting...");
-//         $.ajax({
-//             url : $(this).attr('action'),
-//             type: "POST",
-//             data: $(this).serialize(),
-//             success: function (data) {
-//                 $(".grant-application").html(data);
-//             }
-//         });
-//     });
-
-//     $('.page-signupformpage form.sign-up-form').each(function() {
-//         bindSignUpFormPageForm(this)
-//     });
-
-//     // main blur // Slows down the browser too much
-//     //$('.sign-up').on('click', function() {
-//     //  $('main').toggleClass('blur');
-//    //});
-
-//     // Dropdown menus for mobile
-//     $('.dropdown').click(function() {
-//         $('.dropdown').toggleClass('open');
-//         $('.popular-tags .closed').slideToggle();
-//     });
-//     var querystring = window.location.search;
-//     if ((querystring.indexOf('tag') > 0) && ($('.dropdown').css('display') == 'block')) {
-//         $('.dropdown').addClass('open');
-//         $('.popular-tags .closed').slideToggle();
-//     }
-
-//     // Sticky elements
-//     $('.pick').each(function(){
-//         var $stickyEl = $('.pick');
-//         var elTop = $stickyEl.length && $stickyEl.offset().top - 4;
-
-//         $stickyEl.toggleClass('fixed', $window.scrollTop() > elTop);
-
-//         $window.scroll(function() {
-//             $stickyEl.toggleClass('fixed', $window.scrollTop() > elTop);
-//         });
-//     });
-
-//     $('.nextprev').each(function(){
-//         var $stickyEl = $('.nextprev');
-//         var elTop = $stickyEl.length && $stickyEl.offset().top - 10;
-
-//         $stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
-
-//         $window.scroll(function() {
-//             $stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
-//         });
-//     });
-
-//     // fitVids
-//     $('.responsive-object').fitVids();
-
-//     // get some stats about the video
-//     $('.responsive-object').each(function() {
-//         var $this = $(this);
-//         var iframe = $('iframe', $this)[0];
-//         var player = $f(iframe);
-
-//         $(document).on('click tap', '.poster, .play', function(){
-//             player.api('play');
-//         });
-
-//         player.addEvent('ready', function() {
-//             player.addEvent('play', function(id){
-//                 $('.play').hide();
-//                 $('.breakpoint-medium header, .breakpoint-medium .hero-video h1').animate({
-//                     opacity: 0,
-//                 }, 500, function() {});
-//                 $('.poster').fadeOut(300);
-//                 ga('send', 'event', 'video', 'played', id);
-//             });
-//             player.addEvent('pause', function(id){
-//                 $('header').animate({
-//                     opacity: 1,
-//                 }, 500, function() {});
-//                 ga('send', 'event', 'video', 'paused', id);
-//             });
-//             player.addEvent('finish', function(id){
-//                  $('header').animate({
-//                     opacity: 1,
-//                 }, 500, function() {});
-//                 $('.poster').fadeIn(500);
-//                 ga('send', 'event', 'video', 'finished', id);
-//             });
-//         });
-//     });
-
-//     //featherlight used for lightboxes in streamfield-enabled pages
-//     $('.gallery').featherlightGallery();
-
-//     $(function(){
-
-//         var inOutState = function( trigger, target, speed ){
-
-//             var $trigger    = $( trigger ),
-//                 inClass     = 'mouseEnter',
-//                 outClass    = 'mouseLeave',
-//                 resetClass  = 'reset',
-//                 speed       = speed ? speed : 500,
-//                 state        = {
-//                     busy : false
-//                 }
-
-//             $trigger.each(function(){
-
-//                 var $item    = $( this ),
-//                     $target = $item.find( target );
-
-//                 $item.on( 'mouseenter', function(){
-//                     if( state.busy ){ return false; };
-//                     $target.addClass( inClass );
-//                 });
-
-//                 $item.on( 'mouseleave', function(){
-
-//                     state.busy = true;
-//                     $target.addClass( outClass );
-
-//                     $target.removeClass( inClass );
-//                     setTimeout(function(){
-//                         $target.addClass( resetClass );
-//                         $target.removeClass( outClass );
-//                         setTimeout(function(){
-//                             $target.removeClass( resetClass );
-//                             setTimeout( function(){
-//                                 state.busy = false;
-//                             }, 10);
-//                         }, 5);
-//                     }, speed)
-
-//                 });
-
-//             });
-
-//         };
-
-
-//         inOutState( 'button', '.rule' );
-
-//     });
-// });
