7967169e7b2e0a0e754b03bb769bf548f382cbd6
Mikalai Radchuk <radchuk.m@gmail.com>
2017-03-20 12:01:47 +0300

    Upgrade to Wagtail 1.9

--- tbx/core/models.py
+++ tbx/core/models.py
@@ -622,8 +622,8 @@ class FeaturedPagesBlock(StructBlock):
 class SignUpFormPageBlock(StructBlock):
     page = PageChooserBlock('torchbox.SignUpFormPage')
 
-    def get_context(self, value):
-        context = super(SignUpFormPageBlock, self).get_context(value)
+    def get_context(self, value, parent_context=None):
+        context = super(SignUpFormPageBlock, self).get_context(value, parent_context)
         context['form'] = value['page'].sign_up_form_class()
 
         return context
