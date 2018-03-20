fe46d44a778e3adcf092e4975ea512bda1db1292
Nick Smith <nick.smith@torchbox.com>
2018-03-20 16:37:48 +0000

    Remove Hallo.js whitelisted elements

--- tbx/core/wagtail_hooks.py
+++ tbx/core/wagtail_hooks.py
@@ -2,19 +2,10 @@ from django.utils.html import format_html_join, format_html
 from django.conf import settings
 
 from wagtail.core import hooks
-from wagtail.core.whitelist import allow_without_attributes
 
 from wagtail.contrib.modeladmin.options import ModelAdminGroup, ModelAdmin, modeladmin_register
 
 from .models import GoogleAdGrantApplication, SignUpFormPageResponse
-
-
-@hooks.register('construct_whitelister_element_rules')
-def whitelister_element_rules():
-    return {
-        'blockquote': allow_without_attributes,
-        'span': allow_without_attributes
-    }
 
 
 @hooks.register('insert_editor_js')
