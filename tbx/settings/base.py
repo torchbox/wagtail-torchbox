abd1f23ecdc3f7aeb812e45b09583498a8405a8f
Beth Menzies <beth@incuna.com>
2018-07-03 16:01:33 +0100

    undo change to base.py

--- tbx/settings/base.py
+++ tbx/settings/base.py
@@ -155,10 +155,6 @@ STATICFILES_DIRS = [
 ]
 
 
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, 'static_compiled'),
-]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
