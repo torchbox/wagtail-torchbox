50c567d5187ced36902af5b1beb6700d2cc06d7a
Tomasz Knapik <tomasz.knapik@torchbox.com>
2018-08-22 11:13:34 +0100

    Check if BASE_URL set in the environment first

--- tbx/settings/base.py
+++ tbx/settings/base.py
@@ -334,7 +334,8 @@ WAGTAIL_SITE_NAME = "Torchbox"
 
 FAVICON_STATIC_PATH = 'images/favicon.ico'
 
-BASE_URL = 'https://{}'.format(env['PRIMARY_HOST'])
+if 'PRIMARY_HOST' in env:
+    BASE_URL = 'https://{}'.format(env['PRIMARY_HOST'])
 
 
 # Override the Image class used by wagtailimages with a custom one
