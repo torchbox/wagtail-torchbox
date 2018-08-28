c96bbd14fb2724e98567e0c83ddb51537f709e6b
Tomasz Knapik <tomasz.knapik@torchbox.com>
2018-08-28 20:06:16 +0100

    Serve files directly from S3

--- tbx/core/wagtail_hooks.py
+++ tbx/core/wagtail_hooks.py
@@ -1,5 +1,14 @@
+from django.core.files.storage import get_storage_class
+from django.shortcuts import redirect
+from django.utils.cache import add_never_cache_headers
+
+from storages.backends.s3boto3 import S3Boto3Storage
 from wagtail.contrib.modeladmin.options import (ModelAdmin, ModelAdminGroup,
                                                 modeladmin_register)
+from wagtail.core import hooks
+from wagtail.documents.models import document_served, get_document_model
+
+from tbx.storages.backends import S3Boto3StorageWithQuerystring
 
 from .models import GoogleAdGrantApplication, SignUpFormPageResponse
 
@@ -30,3 +39,23 @@ class SubmissionsModelAdminGroup(ModelAdminGroup):
 
 
 modeladmin_register(SubmissionsModelAdminGroup)
+
+
+@hooks.register('before_serve_document', order=100)
+def serve_document_from_s3(document, request):
+    # Skip this hook if not using django-storages boto3 backend.
+    if not issubclass(get_storage_class(), S3Boto3Storage):
+        return
+
+    # Send document_served signal.
+    document_served.send(sender=get_document_model(), instance=document,
+                         request=request)
+
+    # Generate signed URL so it can have private ACL on S3.
+    file_url = S3Boto3StorageWithQuerystring().url(document.file.name)
+
+    # Generate redirect response and add never_cache headers.
+    response = redirect(file_url)
+    del response['Cache-control']
+    add_never_cache_headers(response)
+    return response
