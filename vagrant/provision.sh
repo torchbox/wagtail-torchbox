1ee490b109a0f1d92303acf2e03d34214c07f778
Dan Braghis <dan.braghis@torchbox.com>
2018-09-13 16:02:36 +0100

    Tweak pip upgrades

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -15,15 +15,19 @@ set +e
 su - vagrant -c "createdb $PROJECT_NAME"
 set -e
 
-
 # Set up virtual environment
 su - vagrant -c "virtualenv --python=python3 $VIRTUALENV_DIR"
 
 su - vagrant -c "echo $PROJECT_DIR > $VIRTUALENV_DIR/.project"
 
 
+# Upgrade PIP itself
+su - vagrant -c "$PIP install --upgrade pip"
+
+# Upgrade setuptools (for example html5lib needs 1.8.5+)
+su - vagrant -c "$PIP install --upgrade six setuptools"
+
 # Install PIP requirements
-su - vagrant -c "$PIP install pip --upgrade"
 su - vagrant -c "$PIP install -r $PROJECT_DIR/requirements.txt"
 
 # Install Fabric 2
