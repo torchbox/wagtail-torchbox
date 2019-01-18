7b34e610ddd941822f36004684a47d29b59ec865
Karl Hobley <karl@torchbox.com>
2019-01-18 16:44:24 +0000

    Add author snippet

--- tbx/people/models.py
+++ tbx/people/models.py
@@ -1,13 +1,17 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.dispatch import receiver
 from django.utils.functional import cached_property
 
 from modelcluster.fields import ParentalKey
 from wagtail.admin.edit_handlers import (FieldPanel, InlinePanel,
-                                         MultiFieldPanel)
+                                         MultiFieldPanel, PageChooserPanel)
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Orderable, Page
+from wagtail.core.signals import page_published
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
+from wagtail.snippets.models import register_snippet
 
 from tbx.core.models import ContactFields, RelatedLink
 
@@ -88,3 +92,55 @@ class PersonIndexPage(Page):
         FieldPanel('senior_management_intro', classname="full"),
         FieldPanel('team_intro', classname="full"),
     ]
+
+
+# An author snippet which keeps a copy of a person's details in case they leave and their page is unpublished
+# Could also be used for external authors
+@register_snippet
+class Author(index.Indexed, models.Model):
+    person_page = models.OneToOneField('people.PersonPage', on_delete=models.SET_NULL, null=True, blank=True, related_name='+')
+
+    name = models.CharField(max_length=255, blank=True)
+    role = models.CharField(max_length=255, blank=True)
+    image = models.ForeignKey(
+        'torchbox.TorchboxImage',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    def update_manual_fields(self, person_page):
+        self.name = person_page.title
+        self.role = person_page.role
+        self.image = person_page.image
+
+    def clean(self):
+        if not self.person_page and not self.name:
+            raise ValidationError({'person_page': "You must set either 'Person page' or 'Name'"})
+
+        if self.person_page:
+            self.update_manual_fields(self.person_page)
+
+    def __str__(self):
+        return self.name
+
+    search_fields = [
+        index.SearchField('name'),
+    ]
+
+    panels = [
+        PageChooserPanel('person_page'),
+        MultiFieldPanel([
+            FieldPanel('name'),
+            FieldPanel('role'),
+            ImageChooserPanel('image'),
+        ], "Manual fields"),
+    ]
+
+
+@receiver(page_published, sender=PersonPage)
+def update_author_on_page_publish(instance, **kwargs):
+    author, created = Author.objects.get_or_create(person_page=instance)
+    author.update_manual_fields(instance)
+    author.save()
