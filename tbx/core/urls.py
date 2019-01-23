a49b48d275e62f3b64f643aed78d051fee2f5283
Karl Hobley <karl@torchbox.com>
2019-01-23 11:32:17 +0000

    Update URL configs

--- tbx/core/urls.py
+++ tbx/core/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url
+from django.urls import path
 
 from tbx.blog.feeds import BlogFeed, PlanetDrupalFeed
 from tbx.core import views
 
 urlpatterns = [
-    url(r'^blog/feed/$', BlogFeed(), name='blog_feed'),
-    url(r'^newsletter-subscribe', views.newsletter_subsribe),
-    url(
-        r'^blog/feed/planet_drupal/$',
+    path('blog/feed/', BlogFeed(), name='blog_feed'),
+    path('newsletter-subscribe', views.newsletter_subsribe),
+    path(
+        'blog/feed/planet_drupal/',
         PlanetDrupalFeed(),
         name='planet_drupal_feed'
     ),
