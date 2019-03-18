a374d949a2b0a03e8b64b50ded845fc793adfc4b
Karl Hobley <karl@torchbox.com>
2019-03-18 14:46:35 +0000

    Remove all frontend URLs

--- tbx/urls.py
+++ tbx/urls.py
@@ -1,59 +1,35 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-from django.views.decorators.cache import never_cache
 from django.views.decorators.vary import vary_on_headers
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 
 from wagtail.admin import urls as wagtailadmin_urls
-from wagtail.contrib.sitemaps.views import sitemap
-from wagtail.core import urls as wagtail_urls
-from wagtail.core.models import Page
-from wagtail.documents import urls as wagtaildocs_urls
 from wagtail.utils.urlpatterns import decorate_urlpatterns
 from wagtail_review import urls as wagtailreview_urls
 
 from tbx.core import urls as torchbox_urls
 from tbx.core.utils.cache import get_default_cache_control_decorator
-from tbx.core.views import favicon, robots
 
 private_urlpatterns = [
     path('django-admin/', admin.site.urls),
     path('admin/', include(wagtailadmin_urls)),
-] + decorate_urlpatterns([
-    path('documents/', include(wagtaildocs_urls)),
-], never_cache)
-
-urlpatterns = [
-    path('sitemap.xml', sitemap),
-    path('favicon.ico', favicon),
-    path('robots.txt', robots),
 ]
 
-
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-    from django.views.generic import TemplateView
-
-    # Serve static and media files from development server
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-    # Add views for testing 404 and 500 templates
-    urlpatterns += [
-        path('test404/', TemplateView.as_view(template_name='404.html')),
-        path('test500/', TemplateView.as_view(template_name='500.html')),
-    ]
-
-
-urlpatterns += [
+urlpatterns = [
     path('review', include(wagtailreview_urls)),
     path('', include(torchbox_urls)),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
 
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Set public URLs to use public cache.
 urlpatterns = decorate_urlpatterns(urlpatterns,
@@ -68,11 +44,5 @@ urlpatterns = decorate_urlpatterns(
                     'Accept-Encoding')
 )
 
-Page.serve = get_default_cache_control_decorator()(Page.serve)
-
 # Join private and public URLs.
-urlpatterns = private_urlpatterns + urlpatterns + [
-    # Add Wagtail URLs at the end.
-    # Wagtail cache-control is set on the page models's serve methods.
-    path(r'', include(wagtail_urls)),
-]
+urlpatterns = private_urlpatterns + urlpatterns
