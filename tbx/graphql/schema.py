d74b054cc5c59a55664eb4e7f902e2f8dbe0dedf
Tomasz Knapik <tomasz.knapik@torchbox.com>
2019-03-25 12:33:45 +0000

    Delete service field from service page schema
    https://projects.torchbox.com/projects/tbxcom-redesign/tickets/136

--- tbx/graphql/schema.py
+++ tbx/graphql/schema.py
@@ -411,7 +411,6 @@ class SubServicePageObjectType(BaseServicePageObjectType):
 
 
 class ServicePageObjectType(BaseServicePageObjectType):
-    service = graphene.Field(ServiceObjectType)
     sub_service_pages = graphene.List(SubServicePageObjectType)
 
     def resolve_sub_service_pages(self, info):
