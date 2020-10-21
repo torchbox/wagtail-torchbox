c47626fa303de6d69de2477b1684c1b18e4a8c78
nicklee <nick@nicklee.co.uk>
2020-10-21 09:29:55 +0100

    Enabling pattern library in dev

--- tbx/settings/dev.py
+++ tbx/settings/dev.py
@@ -7,6 +7,9 @@ SECURE_SSL_REDIRECT = False
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'CHANGEME!!!'
 
+# Enable FE component library
+PATTERN_LIBRARY_ENABLED = True
+
 INTERNAL_IPS = ('127.0.0.1', '10.0.2.2')
 
 BASE_URL = 'http://localhost:8000'
@@ -17,6 +20,10 @@ PREVIEW_URL = 'http://localhost:8001/preview'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 AUTH_PASSWORD_VALIDATORS = []
+
+# Enable Wagtail's style guide in Wagtail's settings menu.
+# http://docs.wagtail.io/en/stable/contributing/styleguide.html
+INSTALLED_APPS += ["wagtail.contrib.styleguide"]  # noqa
 
 # Set URL for the preview iframe. Should point at Gatsby.
 PREVIEW_URL = 'http://localhost:8003/preview/'
