236554171cc061d8305756b4b77981d549167cbb
nicklee <nick@nicklee.co.uk>
2020-10-22 16:14:45 +0100

    Adding social sharing shiz

--- tbx/core/templatetags/util_tags.py
+++ tbx/core/templatetags/util_tags.py
@@ -1,8 +1,18 @@
 from django import template
 from django.utils.text import camel_case_to_spaces, slugify
 
+from tbx.core.utils.models import SocialMediaSettings
+
 register = template.Library()
 
+
+# Social text
+@register.filter(name="social_text")
+def social_text(page, site):
+    try:
+        return page.social_text
+    except AttributeError:
+        return SocialMediaSettings.for_site(site).default_sharing_text
 
 # Get widget type of a field
 @register.filter(name="widget_type")
