fd71560e09796cf7887611b1d5b86b02a58c2f4b
nicklee <nick@nicklee.co.uk>
2020-10-28 11:26:43 +0000

    isort

--- tbx/core/utils/models.py
+++ tbx/core/utils/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from wagtail.admin.edit_handlers import (FieldPanel, MultiFieldPanel)
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
 from wagtail.contrib.settings.models import BaseSetting, register_setting
 from wagtail.images.edit_handlers import ImageChooserPanel
 
