48db2d4528416ec6010e43514266c38a85eade52
nicklee <nick@nicklee.co.uk>
2020-10-29 11:00:24 +0000

    Cookies

--- tbx/static_src/javascript/main.js
+++ tbx/static_src/javascript/main.js
@@ -1,1 +1,12 @@
+import '@babel/polyfill';
+
+import CookieWarning from './components/cookie-message';
+import './components/sticky-point';
+
 import '../sass/main.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+    /* eslint-disable no-new */
+    const cookie = document.querySelector(CookieWarning.selector());
+    new CookieWarning(cookie);
+});
