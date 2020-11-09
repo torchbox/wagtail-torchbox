e2a5dad415b107b1cf81688e151600b67e4a23e7
Brian Xu <brian.xu@torchbox.com>
2020-11-09 19:00:11 +0800

    See More Posts JS:  Now can send Ajax Request and get html response

--- tbx/static_src/javascript/components/see-more-posts.js
+++ tbx/static_src/javascript/components/see-more-posts.js
@@ -1,58 +1,69 @@
 class SeeMorePosts {
-    static selector() {
-        return '[data-posts-destination]';
+  static selector() {
+    return "[data-posts-destination]";
+  }
+
+  constructor(node) {
+    this.node = node;
+    this.seeMoreButton = document.querySelector("[data-posts-fetch-target]");
+    this.target = this.node.dataset.postsDestination;
+    this.targetNode = document.querySelector(
+      `[data-posts-destination="${this.target}"]`
+    );
+    console.log(this.targetNode);
+    this.url = new URL(this.node.baseURI);
+    this.params = new URLSearchParams(this.url.search);
+    this.page = parseInt(this.params.get("page")) || 1;
+    this.filter = this.params.get("filter");
+
+    this.bindEvents();
+  }
+
+  loadMorePostsAjax() {
+    const targetNode = this.targetNode;
+    // page counter
+    let pageCtr = this.page;
+    ++pageCtr;
+
+    // build request object
+    const request = new Request(this.url, {
+      method: "GET",
+      data: { filter: this.filter, page: pageCtr },
+      headers: new Headers({
+        "X-Requested-With": "XMLHttpRequest",
+      }),
+    });
+
+    fetch(request)
+      .then(function (response) {
+        // parse to html
+        return response.text();
+      })
+      .then(function (html) {
+        // Here you get the data to modify as you please
+        console.log(html);
+      })
+      .catch(function (error) {
+        // If there is any error you will catch them here
+        console.log(error);
+        --pageCtr;
+      });
+
+    // update counter
+    if (pageCtr != this.page) {
+      this.page = pageCtr;
+    }
+  }
+
+  bindEvents() {
+    if (!this.seeMoreButton) {
+      return;
     }
 
-    constructor(node) {
-        this.seeMoreButton = document.querySelector('[data-posts-fetch-target]');
-        this.url= new URL(window.location);
-        this.params=new URLSearchParams(this.url.search);
-        this.page=parseInt(this.params.get('page')) || 1;
-        this.filter=this.params.get('filter');
-        this.xmlhttp=null;
-
-        this.bindEvents();
-    }
-
-    loadMorePostsAjax(){
-        this.page+=1;
-        this.xmlhttp = new XMLHttpRequest();
-        this.xmlhttp.onreadystatechange = this.loadMorePostsAjaxCallBack;
-
-        //set parameters
-        this.params = new URLSearchParams();
-        if (this.filter !=null){
-            this.params.set('filter', this.filter);
-        }
-        this.params.set('page', this.page);
-        this.url.search = this.params.toString();
-
-
-        this.xmlhttp.open("GET", this.url, true);
-        // set header to make it AjaxRequest
-        this.xmlhttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-        this.xmlhttp.send();
-    }
-
-    loadMorePostsAjaxCallBack(){
-      if (this.readyState == 4 && this.status == 200) {
-      // if success
-        console.log(this.responseText);
-        }else if(this.readyState == 4){
-        // if error
-        this.page-=1;
-        }
-    }
-
-    bindEvents() {
-        if (!this.seeMoreButton) {
-            return;
-        }
-
-        this.seeMoreButton.addEventListener('click', (event) =>
-            this.loadMorePostsAjax(event),
-        );
-    }
+    this.seeMoreButton.addEventListener("click", (event) =>
+      this.loadMorePostsAjax(event)
+    );
+  }
 }
 
 export default SeeMorePosts;
