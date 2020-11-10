8a96f4be08a51e9a258d58995205a14b795cdda0
Brian Xu <brian.xu@torchbox.com>
2020-11-10 17:07:38 +0800

    See More JS: Now can handle errors

--- tbx/static_src/javascript/components/see-more-posts.js
+++ tbx/static_src/javascript/components/see-more-posts.js
@@ -1,83 +1,90 @@
-class SeeMorePosts {
-  static selector() {
-    return "[data-posts-destination]";
-  }
+class SeeMorePosts
+{
+	static selector()
+	{
+		return "[data-posts-destination]";
+	}
 
-  constructor(node) {
-    this.node = node;
-    this.seeMoreButton = document.querySelector("[data-posts-fetch-target]");
-    this.target = this.node.dataset.postsDestination;
-    this.targetNode = document.querySelector(
-      `[data-posts-destination="${this.target}"]`
-    );
+	constructor(node)
+	{
+		this.node = node;
+		this.seeMoreButton = document.querySelector("[data-posts-fetch-target]");
+		this.target = this.node.dataset.postsDestination;
+		this.targetNode = document.querySelector(
+			`[data-posts-destination="${this.target}"]`
+		);
 
-    this.url = new URL(this.node.baseURI);
-    this.params = new URLSearchParams(this.url.search);
-    this.page = parseInt(this.params.get("page")) || 1;
-    this.filter = this.params.get("filter");
+		this.url = new URL(this.node.baseURI);
+		this.params = new URLSearchParams(this.url.search);
+		this.nextPage = 2;
+		this.filter = this.params.get("filter");
 
-    this.bindEvents();
-  }
+		this.bindEvents();
+	}
 
-  loadMorePostsAjax() {
-    const targetNode = this.targetNode;
-    // page counter
-    let pageCtr = this.page;
-    ++pageCtr;
+	loadMorePostsAjax()
+	{
+		//set parameters
+		this.params = new URLSearchParams();
+		if (this.filter != null)
+		{
+			this.params.set('filter', this.filter);
+		}
+		this.params.set('page', this.nextPage);
+		this.url.search = this.params.toString();
 
-    //set parameters
-    this.params = new URLSearchParams();
-    if (this.filter != null) {
-      this.params.set('filter', this.filter);
-    }
-    this.params.set('page', pageCtr);
-    this.url.search = this.params.toString();
+		// build request object
+		const request = new Request(this.url,
+		{
+			method: "GET",
+			headers: new Headers(
+			{
+				"X-Requested-With": "XMLHttpRequest",
+			}),
+		});
 
-    // build request object
-    const request = new Request(this.url, {
-      method: "GET",
-      headers: new Headers({
-        "X-Requested-With": "XMLHttpRequest",
-      }),
-    });
+		fetch(request)
+			.then(function (response)
+			{
+				if (response.ok)
+				{
+					// parse to html
+					return response.text();
+				}
+				throw new Error(response.status + ": " + response.statusText);
+			})
+			.then(function (html)
+			{
+				if (html.trim().length > 0)
+				{
+					this.targetNode.lastElementChild.insertAdjacentHTML('afterend', html);
+					this.nextPage += 1;
+				}
+				else
+				{
+					// handle empty response
+					console.log("empty response")
+				}
+			}.bind(this))
+			.catch(function (error)
+			{
+				// If there is any error you will catch them here
+				console.log(error);
+			});
 
-    fetch(request)
-      .then(function(response) {
-        // parse to html
-        if (response.ok) {
-          return response.text();
-        } else {
-          throw new Error(response.status + ": " + response.statusText);
-        }
+	}
 
-      })
-      .then(function(html) {
-        // Here you get the data to modify as you please
-        targetNode.lastElementChild.insertAdjacentHTML('afterend', html);
+	bindEvents()
+	{
+		if (!this.seeMoreButton)
+		{
+			return;
+		}
 
-      })
-      .catch(function(error) {
-        // If there is any error you will catch them here
-        console.log(error);
-        --pageCtr;
-      });
-
-    // update counter
-    if (pageCtr != this.page) {
-      this.page = pageCtr;
-    }
-
-  }
-
-  bindEvents() {
-    if (!this.seeMoreButton) {
-      return;
-    }
-
-    this.seeMoreButton.addEventListener("click", (event) =>
-      this.loadMorePostsAjax(event)
-    );
-  }
+		this.seeMoreButton.addEventListener("click", (event) =>
+			this.loadMorePostsAjax(event)
+		);
+	}
 }
 
 export default SeeMorePosts;
