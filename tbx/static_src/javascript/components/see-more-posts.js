bed595e1988c7d79672f537d7430eb87b879e721
Brian Xu <brian.xu@torchbox.com>
2020-11-11 14:05:50 +0800

    See More JS: apply some feedbacks from code review

--- tbx/static_src/javascript/components/see-more-posts.js
+++ tbx/static_src/javascript/components/see-more-posts.js
@@ -1,75 +1,65 @@
 class SeeMorePosts {
-	static selector() {
-		return "[data-fetch-destination]";
-	}
+  static selector() {
+    return "[data-fetch-destination]";
+  }
 
-	constructor(node) {
-		this.node = node;
-		this.target = this.node.dataset.fetchDestination;
-		this.seeMoreButton = document.querySelector(`[data-fetch-target="${this.target}"]`);
-		this.targetNode = document.querySelector(
-			`[data-fetch-destination="${this.target}"]`
-		);
+  constructor(node) {
+    this.node = node;
+    this.target = this.node.dataset.fetchDestination;
+    this.seeMoreButton = document.querySelector(`[data-fetch-target="${this.target}"]`);
+    this.targetNode = document.querySelector(
+      `[data-fetch-destination="${this.target}"]`
+    );
 
-		this.url = new URL(this.node.baseURI);
-		this.params = new URLSearchParams(this.url.search);
-		this.nextPage = 2;
-		this.filter = this.params.get("filter");
+    this.url = new URL(this.node.baseURI);
+    this.params = new URLSearchParams(this.url.search);
+    this.nextPage = 2;
+    this.filter = this.params.get("filter");
 
-		this.bindEvents();
-	}
+    this.bindEvents();
+  }
 
-	loadMorePostsAjax() {
-		//set parameters
-		this.params = new URLSearchParams();
-		if (this.filter != null) {
-			this.params.set('filter', this.filter);
-		}
-		this.params.set('page', this.nextPage);
-		this.url.search = this.params.toString();
+  loadMorePostsAjax() {
+    //set parameters
+    this.params = new URLSearchParams();
+    if (this.filter != null) {
+      this.params.set('filter', this.filter);
+    }
+    this.params.set('page', this.nextPage);
+    this.url.search = this.params.toString();
 
-		// build request object
-		const request = new Request(this.url, {
-			method: "GET",
-			headers: new Headers({
-				"X-Requested-With": "XMLHttpRequest",
-			}),
-		});
+    // build request object
+    const request = new Request(this.url, {
+      method: "GET",
+      headers: new Headers({
+        "X-Requested-With": "XMLHttpRequest",
+      }),
+    });
 
-		fetch(request)
-			.then(function (response) {
-				if (response.ok) {
-					// parse to html
-					return response.text();
-				}
-				throw new Error(response.status + ": " + response.statusText);
-			})
-			.then(function (html) {
-				if (html.trim().length > 0) {
-					this.targetNode.lastElementChild.insertAdjacentHTML('afterend', html);
-					this.nextPage += 1;
-				} else {
-					// handle empty response
-					this.seeMoreButton.parentNode.removeChild(this.seeMoreButton);
-				}
-			}.bind(this))
-			.catch(function (error) {
-				// If there is any error you will catch them here
-				console.log(error);
-			});
+    fetch(request)
+      .then((response) => {
+        if (response.ok) {
+          // parse to html
+          return response.text();
+        }
+        throw new Error(response.status + ": " + response.statusText);
+      })
+      .then((html) => {
+        if (html.trim().length > 0) {
+          this.targetNode.lastElementChild.insertAdjacentHTML('afterend', html);
+          this.nextPage += 1;
+        } else {
+          // handle empty response
+          this.seeMoreButton.parentNode.removeChild(this.seeMoreButton);
+        }
+      })
+  }
 
-	}
-
-	bindEvents() {
-		if (!this.seeMoreButton) {
-			return;
-		}
-
-		this.seeMoreButton.addEventListener("click", (event) =>
-			this.loadMorePostsAjax(event)
-		);
-	}
+  bindEvents() {
+    this.seeMoreButton.addEventListener("click", (event) =>
+      this.loadMorePostsAjax(event)
+    );
+  }
 }
 
 export default SeeMorePosts;
-
