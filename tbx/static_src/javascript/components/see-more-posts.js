bc882add3afa7946d65ba718d2897c1ffb79741c
Brian Xu <brian.xu@torchbox.com>
2020-11-11 15:55:30 +0800

    See More JS: now when reach max pages it will remove see more button

--- tbx/static_src/javascript/components/see-more-posts.js
+++ tbx/static_src/javascript/components/see-more-posts.js
@@ -1,65 +1,63 @@
 class SeeMorePosts {
-  static selector() {
-    return "[data-fetch-destination]";
-  }
-
-  constructor(node) {
-    this.node = node;
-    this.target = this.node.dataset.fetchDestination;
-    this.seeMoreButton = document.querySelector(`[data-fetch-target="${this.target}"]`);
-    this.targetNode = document.querySelector(
-      `[data-fetch-destination="${this.target}"]`
-    );
-
-    this.url = new URL(this.node.baseURI);
-    let params = new URLSearchParams(this.url.search);
-    this.nextPage = 2;
-    this.filter = params.get("filter");
-
-    this.bindEvents();
-  }
-
-  loadMorePostsAjax() {
-    //set parameters
-    let params = new URLSearchParams();
-    if (this.filter != null) {
-      params.set('filter', this.filter);
+    static selector() {
+        return "[data-fetch-target]";
     }
-    params.set('page', this.nextPage);
-    this.url.search = params.toString();
 
-    // build request object
-    const request = new Request(this.url, {
-      method: "GET",
-      headers: new Headers({
-        "X-Requested-With": "XMLHttpRequest",
-      }),
-    });
+    constructor(node) {
+        this.node = node;
+        this.nextPage = 2;
+        this.target = this.node.dataset.fetchTarget;
+        this.seeMoreButton = document.querySelector(`[data-fetch-target="${this.target}"]`);
+        this.targetNode = document.querySelector(
+            `[data-fetch-destination="${this.target}"]`
+        );
+        this.MAX_PAGES = parseInt(this.targetNode.getAttribute('data-fetch-max-pages'));
+        this.url = new URL(this.node.baseURI);
+        this.filter = new URLSearchParams(this.url.search).get("filter");
 
-    fetch(request)
-      .then((response) => {
-        if (response.ok) {
-          // parse to html
-          return response.text();
+        this.bindEvents();
+    }
+
+    loadMorePostsAjax() {
+        //set parameters
+        let params = new URLSearchParams();
+        if (this.filter != null) {
+            params.set('filter', this.filter);
         }
-        throw new Error(response.status + ": " + response.statusText);
-      })
-      .then((html) => {
-        if (html.trim().length > 0) {
-          this.targetNode.lastElementChild.insertAdjacentHTML('afterend', html);
-          this.nextPage += 1;
-        } else {
-          // handle empty response
-          this.seeMoreButton.parentNode.removeChild(this.seeMoreButton);
-        }
-      })
-  }
+        params.set('page', this.nextPage);
+        this.url.search = params.toString();
 
-  bindEvents() {
-    this.seeMoreButton.addEventListener("click", (event) =>
-      this.loadMorePostsAjax(event)
-    );
-  }
+        // build request object
+        const request = new Request(this.url, {
+            method: "GET",
+            headers: new Headers({
+                "X-Requested-With": "XMLHttpRequest",
+            }),
+        });
+
+        fetch(request)
+            .then((response) => {
+                if (response.ok) {
+                    // parse to html
+                    return response.text();
+                }
+                throw new Error(response.status + ": " + response.statusText);
+            })
+            .then((html) => {
+                // update list
+                this.targetNode.lastElementChild.insertAdjacentHTML('afterend', html);
+                this.nextPage += 1;
+                if (this.nextPage > this.MAX_PAGES) {
+                    this.seeMoreButton.parentNode.removeChild(this.seeMoreButton);
+                }
+            })
+    }
+
+    bindEvents() {
+        this.seeMoreButton.addEventListener("click", (event) =>
+            this.loadMorePostsAjax(event)
+        );
+    }
 }
 
 export default SeeMorePosts;
