881d83d490eac6de082edb342f0d5fbe7c42db11
Brian Xu <brian.xu@torchbox.com>
2020-11-13 15:09:07 +0800

    Story Block: Expose fields except embed field

--- tbx/core/blocks.py
+++ tbx/core/blocks.py
@@ -102,11 +102,18 @@ class StoryBlock(StreamBlock):
         label="Wide image",
         template="patterns/molecules/streamfield/blocks/wide_image_block.html",
     )
-    bustout = BustoutBlock()
-    pullquote = PullQuoteBlock()
-    raw_html = RawHTMLBlock(label='Raw HTML', icon="code")
+    bustout = BustoutBlock(template="patterns/molecules/streamfield/blocks/bustout_block.html")
+    pullquote = PullQuoteBlock(template="patterns/molecules/streamfield/blocks/pullquote_block.html")
+    raw_html = RawHTMLBlock(
+        label='Raw HTML',
+        icon="code",
+        template="patterns/molecules/streamfield/blocks/raw_html_block.html",
+    )
     embed = EmbedBlock(icon="code")
-    markdown = MarkdownBlock(icon="code")
+    markdown = MarkdownBlock(
+        icon="code",
+        template="patterns/molecules/streamfield/blocks/raw_html_block.html",
+    )
 
     class Meta:
         template = "patterns/molecules/streamfield/stream_block.html"
