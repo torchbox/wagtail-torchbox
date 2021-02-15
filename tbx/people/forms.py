9f5d26f31bba678f06f24ca5f5309d27315a0053
Brian Xu <brian.xu@torchbox.com>
2021-02-15 13:34:57 +0800

    run black

--- tbx/people/forms.py
+++ tbx/people/forms.py
@@ -4,13 +4,14 @@ from tbx.people import models
 
 
 class ContactForm(WagtailAdminPageForm):
-
     def clean_default_contact(self):
-        default_contact = self.cleaned_data['default_contact']
+        default_contact = self.cleaned_data["default_contact"]
 
         # If user wants to enable the default contact option
         if default_contact:
             # Make sure only one default contact existing
-            models.Contact.objects.filter(default_contact=True).update(default_contact=False)
+            models.Contact.objects.filter(default_contact=True).update(
+                default_contact=False
+            )
 
         return default_contact
