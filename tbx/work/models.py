e98ab175d56d2092271617b63cebbdb16011df0e
Brian Xu <brian.xu@torchbox.com>
2021-02-16 20:27:29 +0800

    add date field in work page

--- tbx/work/models.py
+++ tbx/work/models.py
@@ -67,6 +67,7 @@ class WorkPage(Page):
 
     parent_page_types = ["WorkIndexPage"]
 
+    date = models.DateField("Post date")
     body = StreamField(StoryBlock())
     body_word_count = models.PositiveIntegerField(null=True, editable=False)
     homepage_image = models.ForeignKey(
@@ -137,6 +138,7 @@ class WorkPage(Page):
         FieldPanel("title", classname="full title"),
         FieldPanel("client", classname="client"),
         InlinePanel("authors", label="Author"),
+        FieldPanel("date"),
         StreamFieldPanel("body"),
         ImageChooserPanel("homepage_image"),
         InlinePanel("screenshots", label="Screenshots"),
@@ -181,7 +183,7 @@ class WorkIndexPage(Page):
         work_pages = WorkPage.objects.descendant_of(self).live()
 
         # Order by most recent date first
-        work_pages = work_pages.order_by("-first_published_at", "pk")
+        work_pages = work_pages.order_by("-date", "pk")
 
         return work_pages
 
