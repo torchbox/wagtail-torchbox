008c6da4200663ba1b9748dc6552944e6eb2bf87
Brian Xu <brian.xu@torchbox.com>
2021-02-17 23:09:07 +0800

    run black and flake 8

--- tbx/work/migrations/0022_workpage_date.py
+++ tbx/work/migrations/0022_workpage_date.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('work', '0021_update_streamblock_templates'),
+        ("work", "0021_update_streamblock_templates"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='workpage',
-            name='date',
-            field=models.DateField(null=True, verbose_name='Post date'),
+            model_name="workpage",
+            name="date",
+            field=models.DateField(null=True, verbose_name="Post date"),
         ),
     ]
