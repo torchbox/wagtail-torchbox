7d86b58ed1529234595634be1b342e43b712fbf0
Rich <rich.brennan@torchbox.com>
2021-03-24 16:13:06 +0000

    [328] black format

--- tbx/services/models.py
+++ tbx/services/models.py
@@ -182,10 +182,7 @@ class BaseServicePageKeyPoint(models.Model):
 
 
 class BaseServicePageClientLogo(models.Model):
-    image = models.ForeignKey(
-        "torchbox.TorchboxImage",
-        on_delete=models.CASCADE,
-    )
+    image = models.ForeignKey("torchbox.TorchboxImage", on_delete=models.CASCADE,)
 
     panels = [
         ImageChooserPanel("image"),
@@ -196,10 +193,7 @@ class BaseServicePageClientLogo(models.Model):
 
 
 class BaseServicePageUSAClientLogo(models.Model):
-    image = models.ForeignKey(
-        "torchbox.TorchboxImage",
-        on_delete=models.CASCADE,
-    )
+    image = models.ForeignKey("torchbox.TorchboxImage", on_delete=models.CASCADE,)
 
     panels = [
         ImageChooserPanel("image"),
