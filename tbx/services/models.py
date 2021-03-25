a0304825978b9ef99c204460d70fcc6e35f3e4bb
Rich <rich.brennan@torchbox.com>
2021-03-25 16:26:07 +0000

    [329] black formatting

--- tbx/services/models.py
+++ tbx/services/models.py
@@ -190,10 +190,7 @@ class BaseServicePageKeyPoint(models.Model):
 
 
 class BaseServicePageClientLogo(models.Model):
-    image = models.ForeignKey(
-        "torchbox.TorchboxImage",
-        on_delete=models.CASCADE,
-    )
+    image = models.ForeignKey("torchbox.TorchboxImage", on_delete=models.CASCADE,)
 
     panels = [
         ImageChooserPanel("image"),
@@ -204,10 +201,7 @@ class BaseServicePageClientLogo(models.Model):
 
 
 class BaseServicePageUSAClientLogo(models.Model):
-    image = models.ForeignKey(
-        "torchbox.TorchboxImage",
-        on_delete=models.CASCADE,
-    )
+    image = models.ForeignKey("torchbox.TorchboxImage", on_delete=models.CASCADE,)
 
     panels = [
         ImageChooserPanel("image"),
