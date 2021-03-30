c1061afb18bb58cd275fea30fc1869a69c16c216
Rich <rich.brennan@torchbox.com>
2021-03-30 15:35:45 +0100

    [329] black format

--- tbx/people/migrations/0023_merge_20210330_1437.py
+++ tbx/people/migrations/0023_merge_20210330_1437.py
@@ -6,9 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('people', '0022_auto_20210326_1202'),
-        ('people', '0022_culturepage_featured_blogs'),
+        ("people", "0022_auto_20210326_1202"),
+        ("people", "0022_culturepage_featured_blogs"),
     ]
 
-    operations = [
-    ]
+    operations = []
