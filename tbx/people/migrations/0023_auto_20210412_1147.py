2d3c6b6443631246be0fb13a8434c1c0f8cc3782
Chris Lawton <chris.lawton@torchbox.com>
2021-04-12 13:48:16 +0100

    lint black

--- tbx/people/migrations/0023_auto_20210412_1147.py
+++ tbx/people/migrations/0023_auto_20210412_1147.py
@@ -9,17 +9,46 @@ import wagtail.images.blocks
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('people', '0022_auto_20210412_1042'),
+        ("people", "0022_auto_20210412_1042"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='valuespage',
-            options={'verbose_name': 'Values Page'},
+            name="valuespage", options={"verbose_name": "Values Page"},
         ),
         migrations.AddField(
-            model_name='valuespage',
-            name='standout_items',
-            field=wagtail.core.fields.StreamField([('item', wagtail.core.blocks.StructBlock([('subtitle', wagtail.core.blocks.CharBlock()), ('title', wagtail.core.blocks.CharBlock()), ('description', wagtail.core.blocks.TextBlock()), ('image', wagtail.images.blocks.ImageChooserBlock()), ('link', wagtail.core.blocks.StreamBlock([('internal', wagtail.core.blocks.PageChooserBlock()), ('external', wagtail.core.blocks.URLBlock())]))]))], blank=True),
+            model_name="valuespage",
+            name="standout_items",
+            field=wagtail.core.fields.StreamField(
+                [
+                    (
+                        "item",
+                        wagtail.core.blocks.StructBlock(
+                            [
+                                ("subtitle", wagtail.core.blocks.CharBlock()),
+                                ("title", wagtail.core.blocks.CharBlock()),
+                                ("description", wagtail.core.blocks.TextBlock()),
+                                ("image", wagtail.images.blocks.ImageChooserBlock()),
+                                (
+                                    "link",
+                                    wagtail.core.blocks.StreamBlock(
+                                        [
+                                            (
+                                                "internal",
+                                                wagtail.core.blocks.PageChooserBlock(),
+                                            ),
+                                            (
+                                                "external",
+                                                wagtail.core.blocks.URLBlock(),
+                                            ),
+                                        ]
+                                    ),
+                                ),
+                            ]
+                        ),
+                    )
+                ],
+                blank=True,
+            ),
         ),
     ]
