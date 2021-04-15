3d5dea18121a5252f74011312ac7989d9dfe8567
Chris Lawton <chris.lawton@torchbox.com>
2021-04-15 10:36:16 +0100

    add footer teaser links

--- tbx/navigation/models.py
+++ tbx/navigation/models.py
@@ -25,6 +25,19 @@ class LinkColumnWithHeader(blocks.StructBlock):
         template = ("patterns/molecules/navigation/blocks/footer_column.html",)
 
 
+class CardLinkBlock(blocks.StructBlock):
+    page = blocks.PageChooserBlock()
+    title = blocks.CharBlock(
+        help_text="Leave blank to use the page's own title", required=False
+    )
+    description = blocks.CharBlock(
+        help_text="Optional text to appear under the title", required=False
+    )
+
+    class Meta:
+        template = ("patterns/molecules/navigation/blocks/teaser.html",)
+
+
 @register_setting(icon="list-ul")
 class NavigationSettings(BaseSetting, ClusterableModel):
     primary_navigation = StreamField(
@@ -43,10 +56,16 @@ class NavigationSettings(BaseSetting, ClusterableModel):
         blank=True,
         help_text="Single list of elements at the base of the page.",
     )
+    footer_teasers = StreamField(
+        [("link", CardLinkBlock())],
+        blank=True,
+        help_text="Row of links that use prominent styles to standout.",
+    )
 
     panels = [
         StreamFieldPanel("primary_navigation"),
         StreamFieldPanel("secondary_navigation"),
         StreamFieldPanel("footer_navigation"),
         StreamFieldPanel("footer_links"),
+        StreamFieldPanel("footer_teasers"),
     ]
