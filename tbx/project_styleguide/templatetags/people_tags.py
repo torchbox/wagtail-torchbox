3dddb3e43a5456f8275d8feb9ed0f148d688730e
Tibor Leupold <tibor.leupold@torchbox.com>
2021-04-20 18:24:03 -1000

    Fix isort

--- tbx/project_styleguide/templatetags/people_tags.py
+++ tbx/project_styleguide/templatetags/people_tags.py
@@ -1,5 +1,5 @@
-from tbx.people.templatetags.people_tags import register
-
 from pattern_library.monkey_utils import override_tag
+
+from tbx.people.templatetags.people_tags import register
 
 override_tag(register, name="instagrampost")
