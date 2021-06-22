faec20260517e4141e7c444a5f727f15156dbd78
Rich <rich.brennan@torchbox.com>
2021-06-22 16:31:04 +0100

    [312] code format

--- tbx/events/migrations/0001_initial.py
+++ tbx/events/migrations/0001_initial.py
@@ -10,39 +10,76 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('taxonomy', '0005_service_contact_reasons'),
-        ('people', '0024_auto_20210412_1428'),
-        ('wagtailcore', '0062_comment_models_and_pagesubscription'),
+        ("taxonomy", "0005_service_contact_reasons"),
+        ("people", "0024_auto_20210412_1428"),
+        ("wagtailcore", "0062_comment_models_and_pagesubscription"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='EventIndexPage',
+            name="EventIndexPage",
             fields=[
-                ('page_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
+                (
+                    "page_ptr",
+                    models.OneToOneField(
+                        auto_created=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        parent_link=True,
+                        primary_key=True,
+                        serialize=False,
+                        to="wagtailcore.Page",
+                    ),
+                ),
             ],
-            options={
-                'abstract': False,
-            },
-            bases=('wagtailcore.page',),
+            options={"abstract": False,},
+            bases=("wagtailcore.page",),
         ),
         migrations.CreateModel(
-            name='Event',
+            name="Event",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('sort_order', models.IntegerField(blank=True, editable=False, null=True)),
-                ('title', models.CharField(max_length=255)),
-                ('intro', models.TextField(verbose_name='Description')),
-                ('link_external', models.URLField(verbose_name='External link')),
-                ('date', models.DateField(verbose_name='Event date')),
-                ('event_type', models.CharField(max_length=30)),
-                ('author', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='authors', to='people.Author', verbose_name='Host')),
-                ('page', modelcluster.fields.ParentalKey(on_delete=django.db.models.deletion.CASCADE, related_name='events', to='events.EventIndexPage')),
-                ('related_services', modelcluster.fields.ParentalManyToManyField(related_name='events', to='taxonomy.Service')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "sort_order",
+                    models.IntegerField(blank=True, editable=False, null=True),
+                ),
+                ("title", models.CharField(max_length=255)),
+                ("intro", models.TextField(verbose_name="Description")),
+                ("link_external", models.URLField(verbose_name="External link")),
+                ("date", models.DateField(verbose_name="Event date")),
+                ("event_type", models.CharField(max_length=30)),
+                (
+                    "author",
+                    models.ForeignKey(
+                        null=True,
+                        on_delete=django.db.models.deletion.SET_NULL,
+                        related_name="authors",
+                        to="people.Author",
+                        verbose_name="Host",
+                    ),
+                ),
+                (
+                    "page",
+                    modelcluster.fields.ParentalKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="events",
+                        to="events.EventIndexPage",
+                    ),
+                ),
+                (
+                    "related_services",
+                    modelcluster.fields.ParentalManyToManyField(
+                        related_name="events", to="taxonomy.Service"
+                    ),
+                ),
             ],
-            options={
-                'ordering': ['sort_order'],
-                'abstract': False,
-            },
+            options={"ordering": ["sort_order"], "abstract": False,},
         ),
     ]
