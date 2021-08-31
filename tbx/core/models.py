59cef78f30f923e2b5f7984a1c038cfedf376f54
Brian Xu <brian.xu@torchbox.com>
2021-08-31 15:31:17 +0800

    use penels

--- tbx/core/models.py
+++ tbx/core/models.py
@@ -218,7 +218,7 @@ class HomePageFeaturedPost(Orderable):
         related_name="+",
     )
 
-    content_panels = [
+    panels = [
         PageChooserPanel("feature_post", ["blog.BlogPage", "work.WorkPage"]),
     ]
 
