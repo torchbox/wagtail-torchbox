908d202ae075daab682300e43b4a57b424fd247b
Ben Morse <>
2022-01-09 17:23:56 +0000

    fix app name for loggers

--- tbx/settings/base.py
+++ tbx/settings/base.py
@@ -262,7 +262,7 @@ LOGGING = {
         }
     },
     "loggers": {
-        "selfhelp": {"handlers": ["console"], "level": "INFO", "propagate": False},
+        "tbx": {"handlers": ["console"], "level": "INFO", "propagate": False},
         "wagtail": {"handlers": ["console"], "level": "INFO", "propagate": False},
         "django.request": {
             "handlers": ["console"],
