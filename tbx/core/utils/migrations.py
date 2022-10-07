570ef24ef5dd20d90259eda1aff5cd3e182bcf4f
nick.moreton <nick.moreton@torchbox.com>
2022-10-07 11:55:57 +0100

    Remove checking WAGTAIL version

--- tbx/core/utils/migrations.py
+++ tbx/core/utils/migrations.py
@@ -1,8 +1,6 @@
 import json
 from functools import wraps
 
-from wagtail import VERSION as WAGTAIL_VERSION
-
 
 def for_each_page_revision(*model_names):
     """
@@ -24,10 +22,7 @@ def for_each_page_revision(*model_names):
         @wraps(fn)
         def wrapper(apps, schema_editor):
             ContentType = apps.get_model("contenttypes.ContentType")
-            if WAGTAIL_VERSION >= (4, 0):
-                PageRevision = apps.get_model("wagtailcore.Revision")
-            else:
-                PageRevision = apps.get_model("wagtailcore.PageRevision")
+            PageRevision = apps.get_model("wagtailcore.Revision")
 
             content_types = [
                 ContentType.objects.get_for_model(apps.get_model(model_name))
