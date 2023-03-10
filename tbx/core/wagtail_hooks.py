d3fb110a004c8cdef4025c2e2083bd14ea41da8d
Thibaud Colas <thibaudcolas@gmail.com>
2023-03-10 10:14:32 +0000

    Add Hotjar tracking with insert_global_admin_js (#446)

--- tbx/core/wagtail_hooks.py
+++ tbx/core/wagtail_hooks.py
@@ -1,6 +1,8 @@
+from django.conf import settings
 from django.core.files.storage import get_storage_class
 from django.shortcuts import redirect
 from django.utils.cache import add_never_cache_headers
+from django.utils.safestring import mark_safe
 
 from storages.backends.s3boto3 import S3Boto3Storage
 
@@ -33,3 +35,30 @@ def serve_document_from_s3(document, request):
 @hooks.register("construct_settings_menu")
 def hide_main_menu_menu_item(request, menu_items):
     menu_items[:] = [item for item in menu_items if item.name != "main-menu"]
+
+
+@hooks.register("insert_global_admin_js")
+def hotjar_admin_tracking():
+    """
+    Trial Hotjar tracking for the CMS admin. Testing whether
+    this can help with Wagtail core design decisions.
+    """
+    hjid = settings.ADMIN_HOTJAR_SITE_ID
+
+    if not hjid:
+        return ""
+
+    return mark_safe(
+        f"""
+    <script>
+        (function(h,o,t,j,a,r){{
+            h.hj=h.hj||function(){{(h.hj.q=h.hj.q||[]).push(arguments)}};
+            h._hjSettings={{hjid:{hjid},hjsv:6}};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        }})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
+    """
+    )
