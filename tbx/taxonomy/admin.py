14259b2bb57cedc3b355363c2a7a86ad73f18d4f
Lauren <lauren.parsons@torchbox.com>
2023-11-07 09:23:29 +0000

    Remove superfluous TaxonomyModelAdmin parent snippet view class

--- tbx/taxonomy/admin.py
+++ tbx/taxonomy/admin.py
@@ -3,13 +3,11 @@ from wagtail.snippets.views.snippets import SnippetViewSet, SnippetViewSetGroup
 from . import models
 
 
-class TaxonomyModelAdmin(SnippetViewSet):
-    def get_admin_urls_for_registration(self):
-        return super().get_admin_urls_for_registration()
-
-
-class ServiceModelAdmin(TaxonomyModelAdmin):
+class ServiceModelAdmin(SnippetViewSet):
     model = models.Service
+    base_url_path = "taxonomy/service"
+    list_display = ("name", "slug", "sort_order")
+    ordering = ["sort_order"]
 
 
 class TaxonomyModelAdminGroup(SnippetViewSetGroup):
